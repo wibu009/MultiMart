bbb0ba2aa4c9d65c122fd54a8890014d1cceb3c0
wibu009 <kingofdespair69@gmail.com>
2024-07-23 03:22:20 +0700

    update shacdn for ui

--- src/Client/Web/vite.config.ts
+++ src/Client/Web/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import mkcert from 'vite-plugin-mkcert'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mkcert()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })

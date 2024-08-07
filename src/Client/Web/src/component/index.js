5475f6bf9b9db45d7a30403be5ed425ffbd75aa5
wibu009 <kingofdespair69@gmail.com>
2024-08-07 15:24:37 +0700

    Revert "add ReactJS front end"
    This reverts commit 6285645f566996c58cfef5b4e256889531e56373.

--- src/Client/Web/src/component/index.js
+++
@@ -1,4 +0,0 @@
-import { default as ProductCard } from "./ProductCard";
-import { default as Quantity } from "./Quantity";
-
-export { ProductCard, Quantity };
--- src/Client/Web/src/utils/formater.js
+++
@@ -1,6 +0,0 @@
-export const formatter = (number) => {
-  return new Intl.NumberFormat("vi-VN", {
-    style: "currency",
-    currency: "VND",
-  }).format(number);
-};
